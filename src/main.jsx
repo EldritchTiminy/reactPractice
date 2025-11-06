5ca16301aaca5e7df8b7c45dbd21b20c3e0c2962
Tim <tim.duke.314@gmail.com>
2025-11-06 11:39:20 -0600

    Added page for rendering a list.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,67 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+//import App from './App.jsx';
 import Greeting from './Greeting.jsx';
 import {NavBar} from './navbar';
+import {ArmoryCatalog} from './dndweapons';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(
-  <StrictMode>
-    <App />
-    <Greeting />
-    <NavBar />
-  </StrictMode>,
-);
+
+let cCount = 0;
+
+const animals = ["Lion", "Cow", "Snake", "Lizard", "Dragon"];
+
+function clickCounter () {
+  cCount++;
+  console.log("cCount + 1");
+  console.log(cCount);
+  renderPage();
+};
+
+function ListItem (props) {
+  return <li>{props.animal}</li>;
+};
+
+function List(props) {
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return <ListItem key={animal} animal={animal} />;
+      })}
+    </ul>
+  );
+};
+
+function App () {
+  const animals = ["Lion", "Cow", "Snake", "Lizard", "Pangolin"];
+
+  return (
+    <div>
+      <h1>Animals: </h1>
+      <List animals={animals} />
+    </div>
+  );
+};
+
+
+
+function renderPage () {
+  root.render(
+    <StrictMode>
+      <App />
+      <ArmoryCatalog />
+      <Greeting />
+      <NavBar />
+      <div>
+        <p onClick={clickCounter}>{cCount}</p>
+      </div>
+      {animals.map((animal) => {
+        return <li key={animal}>{animal}</li>;
+      })}
+    </StrictMode>,
+  );
+};
+
+renderPage();

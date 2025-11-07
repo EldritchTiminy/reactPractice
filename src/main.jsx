7a741da32955c68e2859f5316dba3444558f7c13
Tim <tim.duke.314@gmail.com>
2025-11-07 09:40:40 -0600

    Deleted preset css files.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-//import App from './App.jsx';
 import Greeting from './Greeting.jsx';
 import {NavBar} from './navbar';
 import {ArmoryCatalog} from './dndweapons';
@@ -44,8 +43,6 @@ function App () {
     </div>
   );
 };
-
-
 
 function renderPage () {
   root.render(
